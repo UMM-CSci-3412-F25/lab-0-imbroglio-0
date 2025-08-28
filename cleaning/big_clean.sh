45f8d76a6b42589a494a94a59039b91ef9183528
Harley Hannahs <hanna537@morris.umn.edu>
2025-08-28 11:38:47 -0500

    Progress on making scratch directory
    Working with creating the scratch directory and deleting the correct files from it

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+tar_archive="$1"
+#taking a compressed tar archive as input
+
+mktemp -d scratch
+#creating temporary directory to do work in
+
+tar -xzf "$tar_archive" -C /Users/hhannahs/Desktop/CSCI/3412/lab-0-imbroglio-0/cleaning/scratch
+#extracting the tar archive
+
+find . | grep "DELETE ME!" | xargs rm -f
+#looking for files that say "DELETE ME!"
+
+#tar -czvf cleaned_"$tar_archive$".tar.gz cleaning -C /scratch/
