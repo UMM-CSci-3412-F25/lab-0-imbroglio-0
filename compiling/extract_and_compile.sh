09cdc019cbc43394406f2a83eb39a9894f13d53c
Harley Hannahs <hanna537@morris.umn.edu>
2025-08-28 10:10:06 -0500

    Labeling extract_and_compile.sh
    Labeling the parts of the shell with comments to reference them later

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 tar -xzf NthPrime.tgz
+//extract files
 
 cd NthPrime || exit
 
 gcc nth_prime.c main.c -o NthPrime -Wall
+//compile C program
 
 ./NthPrime "$@"
+//calling the executable
