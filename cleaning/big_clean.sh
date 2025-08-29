53182326e10cf4e92f512a8cf5ea95015a7ab7c9
Harley Hannahs <hanna537@morris.umn.edu>
2025-08-29 14:43:58 -0500

    Variable Names
    Added quotes to variable names so that they function properly

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -2,20 +2,20 @@
 
 TAR_ARCHIVE=$1
 #taking a compressed tar archive as input
-FOLDER_NAME=$(basename $TAR_ARCHIVE .tgz)
+FOLDER_NAME=$(basename "$TAR_ARCHIVE" .tgz)
 CLEAN_TAR="cleaned_${TAR_ARCHIVE}"
 
 SCRATCH=$(mktemp --directory SCRATCH-XXXXXX)
 #creating temporary directory to do work in
 
-tar -xzf $TAR_ARCHIVE -C $SCRATCH
+tar -xzf "$TAR_ARCHIVE" -C "$SCRATCH"
 #extracting the tar archive
 
-grep -rl "DELETE ME\!" $SCRATCH | xargs rm
+grep -rl "DELETE ME\!" "$SCRATCH" | xargs rm
 #looking for files that say "DELETE ME!" and deleting them
 
-cd $SCRATCH || exit
+cd "$SCRATCH" || exit
 
-tar -czf $CLEAN_TAR $FOLDER_NAME
+tar -czf "$CLEAN_TAR" "$FOLDER_NAME"
 
-mv $CLEAN_TAR ./..
+mv "$CLEAN_TAR" ./..
